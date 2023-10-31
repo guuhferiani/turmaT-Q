6aaac12587565dc64c2a7bb70469b1ca839d3be1
Gustavo Feriani <guuh.feriani@gmail.com>
2023-10-31 20:42:44 -0300

    update javascritp
    turma T-Q

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 const album = document.getElementById('album');
+const casamentos = document.getElementById('casamentos');
+const buffet = document.getElementById('buffet');
 
 const videoAlbum = document.querySelector('#videoAlbum');
+const videoCasamentos = document.querySelector('#videoCasamentos');
+const videoBuffet = document.querySelector('#videoBuffet');
+
 
 album.addEventListener('click', () => {
     album.classList.toggle('active');
     videoAlbum.classList.toggle('active');
+});
+casamentos.addEventListener('click', () => {
+    casamentos.classList.toggle('active');
+    videoCasamentos.classList.toggle('active');
+});
+buffet.addEventListener('click', () => {
+    buffet.classList.toggle('active');
+    videoBuffet.classList.toggle('active');
 });
